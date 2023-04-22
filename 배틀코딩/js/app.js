862984ce6f5b304c140214a0a6a5615122501ea6
sungha0201 <gkgkrla030@gmail.com>
2023-04-22 15:39:19 +0900

    localstorage 전까지 완료

---
+++ 배틀코딩/js/app.js
@@ -0,0 +1,60 @@
+const todoForm = document.querySelector(".todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.querySelector(".todo-list");
+//빈배열 생성 -> 
+
+function saveTodo() {
+  localStorage.setItem(TODO_KEY, JSON.stringify(todos));
+  mode = 'insert';
+}
+//list생성
+function formSubmit(e) {
+  let inputVal = todoInput.value;
+  let newLi = document.createElement("li");
+  let liTextWrap = document.createElement("span");
+  todoList.appendChild(newLi);
+  newLi.appendChild(liTextWrap);
+  liTextWrap.classList.add("text-wrap");
+  liTextWrap.innerText = inputVal;
+
+  const newDeleteBtn = document.createElement('span');
+  newDeleteBtn.classList.add("delete-btn");
+  newDeleteBtn.innerHTML = '<i class="fas fa-solid fa-trash"></i>';
+
+  const newFixBtn = document.createElement('span');
+  newFixBtn.innerHTML = '<i class="fix"></i>';
+
+  newLi.appendChild(newDeleteBtn);
+  newLi.appendChild(newFixBtn);
+  todoInput.value = "";
+
+  newDeleteBtn.addEventListener("click", deleteList);
+  newFixBtn.addEventListener("click", fixList);
+}
+
+//list 수정
+function fixList(e) {
+  let listItem = e.target.parentElement.parentElement;
+  let newInput = document.createElement('input');
+  let liTextWrap = listItem.querySelector(".text-wrap");
+  let newDeleteBtn = listItem.querySelector(".delete-btn");
+
+  targetText = listItem.innerText;
+  liTextWrap.innerText = "";
+  newInput.value = targetText;
+  listItem.insertBefore(newInput, newDeleteBtn);
+
+  newInput.addEventListener("change", function () {
+    liTextWrap.innerText = this.value;
+    newInput.remove();
+  });
+}
+
+//list 삭제
+function deleteList(e) {
+  let targetParent = e.target.parentElement.parentElement;
+  targetParent.remove();
+}
+
+
+todoForm.addEventListener("submit", formSubmit);
